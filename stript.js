9bf2e574f273e4b9487f51bd7d91d363e34b080e
David Cruz Esk <david.cruz.esk@gmail.com>
2024-03-24 19:26:31 -0400

    UPDATE SUNDAY-24-2024

--- stript.js
+++ stript.js
@@ -14,14 +14,85 @@ const bookUrl = document.querySelector('#book-url');
 const price = document.querySelector('#book-price');
 const stockStatus = document.querySelector('#stock-option');
 const submitButton = document.querySelector('#add-book');
+const bookcovers = document.querySelectorAll('.bookcover img');
+const dropDown = document.querySelector('.book-descript-section');
+const lastSection = document.querySelector('#main-section__book-description');
+
+
+//local storage functions ====================
+
+function saveToLocalStorage(key, element) {
+    const serializedElement = JSON.stringify(element.outerHTML);
+    localStorage.setItem(key, serializedElement);
+}
+
+function getFromLocalStorage(key) {
+    const serializedElement = localStorage.getItem(key);
+    const deserializedElement = document.createElement('article');
+    deserializedElement.innerHTML = JSON.parse(serializedElement);
+    return deserializedElement;
+}
+
+//local storage functions ====================
+
+// create book description section function
+function createBookDescriptSection() {
+    //creates the elements
+    const bookDescriptSect = document.createElement('article');
+    const bookDescriptImage = document.createElement('img');
+    bookDescriptSect.appendChild(bookDescriptImage);
+
+    const bookDescriptHeader = document.createElement('header');
+    const bookDescriptH1 = document.createElement('h1');
+    bookDescriptHeader.appendChild(bookDescriptH1);
+    const bookDescriptAuthor = document.createElement('h3');
+    bookDescriptHeader.appendChild(bookDescriptAuthor);
+    bookDescriptSect.appendChild(bookDescriptHeader);
+
+    const bookDescriptParagraph = document.createElement('p');
+    bookDescriptSect.appendChild(bookDescriptParagraph);
+    //finishes creating all elements
+
+    return bookDescriptSect;
+}
+
 
 window.onload = function() {
     //removes any books from the list, when you press the trash icon
     trashImages.forEach(trash => {
         trash.addEventListener('click', function() {
-            trash.parentNode.parentNode.remove();
+            let opacity = 1;
+            let fadeOut = setInterval(function() {
+                if (opacity > 0) {
+                    opacity -= 0.1;
+                    trash.parentNode.parentNode.style.opacity = opacity;
+                    trash.parentNode.parentNode.style.marginLeft += '1px';
+                } else {
+                    clearInterval(fadeOut);
+                    trash.parentNode.parentNode.remove();
+                }
+            }, 50);
         });
     });
+
+    bookcovers.forEach(bookcover => {
+        bookcover.addEventListener('click', function() {
+            lastSection.style.backgroundImage = 'linear-gradient(to top, rgba(255, 255, 255, 0), rgba(255, 255, 255, .6), rgba(255, 255, 255, .6), rgba(255, 255, 255, 0))';
+            dropDown.style.display = 'block';
+            
+
+            let opacity = 0;
+            let fadeOut = setInterval(function() {
+                if (opacity < 1) {
+                    opacity += 0.1;
+                    dropDown.style.opacity = opacity;
+                } else {
+                    clearInterval(fadeOut);
+                }
+            }, 50);
+        });
+    });
+
 
     //This event adds a book and all of its details into the book list.
     submitButton.addEventListener('click', function(e) {
@@ -90,9 +161,20 @@ window.onload = function() {
 
         //appends the entire book into the booklist
         bookList.appendChild(article);
-
+        // saves the article into the locale storage
+        
         trashIcon.addEventListener('click', function() {
-            article.remove();
+            let opacity = 1;
+            let fadeOut = setInterval(function() {
+                if (opacity > 0) {
+                    opacity -= 0.1;
+                    article.style.opacity = opacity;
+                    article.style.marginLeft += '1px';
+                } else {
+                    clearInterval(fadeOut);
+                    article.remove();
+                }
+            }, 50);
         });
     });
 }
